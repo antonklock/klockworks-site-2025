5620e83b5730967f3e3a322059f60e940ae1d9f6
Anton Klock <anton@klockworks.se>
2025-07-10 12:46:31 +0200

    Moving the scroll arrow up a bit on mobiled

--- components/Header/ScrollArrow.tsx
+++ components/Header/ScrollArrow.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const ScrollArrow = () => {
   return (
     <div 
-      className="absolute left-1/2 -translate-x-1/2 flex flex-col items-center text-white bottom-4 pb-4 md:bottom-6 md:pb-6"
+      className="absolute left-1/2 -translate-x-1/2 flex flex-col items-center text-white bottom-12 pb-4 md:bottom-16 md:pb-6"
       style={{
-        bottom: 'max(1rem, env(safe-area-inset-bottom, 1rem))',
+        bottom: 'max(3rem, calc(env(safe-area-inset-bottom, 1rem) + 2rem))',
         paddingBottom: 'max(1rem, env(safe-area-inset-bottom, 1rem))'
       }}
     >
